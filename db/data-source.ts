a3910b995a70013db1529c3b9ae0e8ac3fe469d8
EntitiCoder <nhat.vn@tribox.me>
2024-01-21 22:19:42 +0700

    clone from src initial commit

---
+++ db/data-source.ts
@@ -0,0 +1,20 @@
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+config();
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  // migrations: ['dist/db/migrations/*{.ts,.js}'],
+  migrations: [],
+  logging: false,
+  synchronize: true,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+dataSource.initialize();
+export default dataSource;
